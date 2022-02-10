d44eea4f86bd44b154eb38042a8fb1b3071f4a48
Zhendong Tang <zhendongtt@gmail.com>
2022-02-10 17:00:57 -0500

    [Tag Helper] Using tag helper

---
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 25/Beginning of Chapter/WebApp/TagHelpers/TrTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApp.TagHelpers
+{
+    public class TrTagHelper:TagHelper
+    {
+        public string BgColor { get; set; } = "dark";
+        public string TextColor { get; set; } = "white";
+
+        /// <summary>
+        /// This method id from TagHelper
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="output"></param>
+        public override void Process(TagHelperContext context,TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("class", $"bg-{BgColor} text-center text-{TextColor}");
+        }
+
+    }
+}
