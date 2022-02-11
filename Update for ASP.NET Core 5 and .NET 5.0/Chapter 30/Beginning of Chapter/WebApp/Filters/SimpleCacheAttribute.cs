3698f5ab331f4767e7e88903a5c0847430a15b95
Zhendong Tang <zhendongtt@gmail.com>
2022-02-11 18:21:39 -0500

    [Custom Filter] Synchronous Resource Filter

---
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Filters/SimpleCacheAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+
+namespace WebApp.Filters
+{
+    /// <summary>
+    /// From the Interface :// Summary:
+    //     A filter that surrounds execution of model binding, the action (and filters)
+    //     and the action result (and filters).
+    /// </summary>
+    public class SimpleCacheAttribute : Attribute, IResourceFilter
+    {
+        
+        private Dictionary<PathString, IActionResult> CachedResponses =new Dictionary<PathString, IActionResult>();
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            PathString path=context.HttpContext.Request.Path;
+            if (CachedResponses.ContainsKey(path))
+            {
+                context.Result = CachedResponses[path];
+                CachedResponses.Remove(path); // Why remove it ? 
+            }
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            // Cache the result: context.Result
+            CachedResponses.Add(context.HttpContext.Request.Path, context.Result);
+        }
+    }
+
+}
