133a3a4d8160bdd8c3b5523e2d8ef84d10c29a6e
Zhendong Tang <zhendongtt@gmail.com>
2022-02-11 15:56:53 -0500

    [Custom Filter] Authorization

--- Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Controllers/HomeController.cs
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Controllers/HomeController.cs
@@ -1,20 +1,15 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Filters;
 
 namespace WebApp.Controllers {
-
+    [HttpsOnly]
     public class HomeController : Controller {
 
+        
         public IActionResult Index() {
-            if (Request.IsHttps)
-            {
-                return View("Message","This is the Index action on the Home controller");
-            }
-            else
-            {
-                return new StatusCodeResult(StatusCodes.Status403Forbidden);
-            }
-           
+            return View("Message","This is the Index action on the Home controller");
         }
     }
 }
---
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Filters/HttpsOnlyAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace WebApp.Filters
+{
+    /// <summary>
+    /// Create [HttpsOnly] Authorization Filter
+    /// Be Aware of the Interface we are supposed to inherit.
+    /// </summary>
+    public class HttpsOnlyAttribute : Attribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            // The "context" object almost provide everythis we required
+            if (!context.HttpContext.Request.IsHttps)
+            {
+                // Here only for demo.
+                // In production, would redirect to a meaningful webpage other than 403
+                context.Result = new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+        }
+    }
+}
