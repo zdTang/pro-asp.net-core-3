1535e99127a35aec73148db7749fee36adb2e21d
Zhendong Tang <zhendongtt@gmail.com>
2022-02-12 06:04:20 -0500

    [Custom Filter] Asynchronous Resource Filter

---
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Filters/SimpleCacheAsyncAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebApp.Filters
+{
+    public class SimpleCacheAsyncAttribute : Attribute, IAsyncResourceFilter
+    {
+
+        private Dictionary<PathString, IActionResult> CachedResponses = new Dictionary<PathString, IActionResult>();
+        
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            PathString path=context.HttpContext.Request.Path;
+            if (CachedResponses.ContainsKey(path))
+            {
+                context.Result = CachedResponses[path];
+                CachedResponses.Remove(path);
+            }
+            else
+            {
+                // Get "execContext" after executing "next"
+                ResourceExecutedContext execContext=await next();
+                CachedResponses.Add(path, execContext.Result);
+            }
+        }
+    }
+
+}
