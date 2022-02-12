70fda0b5b10ddce654aca4d327126073b236c679
Zhendong Tang <zhendongtt@gmail.com>
2022-02-12 06:43:27 -0500

    [Custom Filter] Action Filter -- Approach one

--- Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Controllers/HomeController.cs
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Controllers/HomeController.cs
@@ -6,10 +6,15 @@ using WebApp.Filters;
 namespace WebApp.Controllers {
     [HttpsOnly]
     public class HomeController : Controller {
-
-        
+       
         public IActionResult Index() {
             return View("Message","This is the Index action on the Home controller");
+        }
+
+        [ChangeArg]
+        public IActionResult Messages(string message1, string message2 = "none")
+        {
+            return View("Message",$"{message1},{message2}");
         }
     }
 }
