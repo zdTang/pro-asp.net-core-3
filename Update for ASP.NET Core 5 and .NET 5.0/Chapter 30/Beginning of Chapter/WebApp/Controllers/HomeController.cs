d71301e9aa8387729cf9d0af33eb0b04a4212133
Zhendong Tang <zhendongtt@gmail.com>
2022-02-12 07:26:50 -0500

    [Custom Filter] Action filter -- approach three

--- Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Controllers/HomeController.cs
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using WebApp.Filters;
 
 namespace WebApp.Controllers {
@@ -12,10 +14,26 @@ namespace WebApp.Controllers {
         }
 
         //[ChangeArg]
-        [ChangeArgTwo]
+        //[ChangeArgTwo]
         public IActionResult Messages(string message1, string message2 = "none")
         {
             return View("Message",$"{message1},{message2}");
         }
+
+        // called before Action is invoked
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ActionArguments.ContainsKey("message1"))
+            {
+                context.ActionArguments["message1"] = "New message";
+            }
+           
+        }
+        // called after Action is invoked
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            context.Result = View("Message", "MIKE,TANG");
+        }
+
     }
 }
