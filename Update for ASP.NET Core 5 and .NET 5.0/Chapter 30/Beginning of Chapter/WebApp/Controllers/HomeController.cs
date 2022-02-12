3dcd72df9ecd2e8699fd7956e9be3e3b7867fb7f
Zhendong Tang <zhendongtt@gmail.com>
2022-02-12 10:55:48 -0500

    [Customer Filter] Result Filter -- Approach one

--- Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Controllers/HomeController.cs
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 30/Beginning of Chapter/WebApp/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ using WebApp.Filters;
 
 namespace WebApp.Controllers {
     [HttpsOnly]
+    [ResultDiagnostics]
     public class HomeController : Controller {
        
         public IActionResult Index() {
