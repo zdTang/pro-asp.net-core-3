b765db4754247a06f7e5947cfb4a10a1ab39f41a
Zhendong Tang <zhendongtt@gmail.com>
2022-02-13 16:49:31 -0500

    [MiddleWare] Return Pipeline Path

--- Update for ASP.NET Core 5 and .NET 5.0/Chapter 12/Platform/Startup.cs
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 12/Platform/Startup.cs
@@ -24,16 +24,27 @@ namespace Platform {
             {
                 app.UseDeveloperExceptionPage();
             }
+            // Test middleware work with return pipeline
 
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Custom Middleware1 --coming \n");
+                await next();
+                await context.Response.WriteAsync($"\n Custom Middleware1 --Status Code:{context.Response.StatusCode}");
+            });
+            
+            
+            
             // Create a custom Middle ware: Approach one
             // The downside of this approach is it is not easy to re-use this middleware
             app.Use(async (context, next) =>
             {
                 if(context.Request.Method==HttpMethods.Get && context.Request.Query["custom"]=="true")
                 {
-                    await context.Response.WriteAsync("Custom Middleware \n");
+                    await context.Response.WriteAsync("Custom Middleware2 \n");
                 }
                 await next();
+                await context.Response.WriteAsync($"\n Custom Middleware2 --Status Code:{context.Response.StatusCode}");
             });
             //Add custom middleware: Approach two
             app.UseMiddleware<QueryStringMiddleWare>();
