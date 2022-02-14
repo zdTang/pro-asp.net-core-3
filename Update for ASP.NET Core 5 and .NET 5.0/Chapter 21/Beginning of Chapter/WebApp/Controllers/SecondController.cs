c8df7dc4b306ab61b523ffd86242898d3e07310e
Zhendong Tang <zhendongtt@gmail.com>
2022-02-14 09:44:01 -0500

    [Chapter-21] Shared View

---
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 21/Beginning of Chapter/WebApp/Controllers/SecondController.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class SecondController : Controller
+    {
+        public IActionResult Common()
+        {
+            return View("Common");
+        }
+    }
+}
