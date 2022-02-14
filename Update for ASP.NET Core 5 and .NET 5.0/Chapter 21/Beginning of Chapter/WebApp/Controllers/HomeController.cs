366296052c85b41d34ff77e8cb6c60286f120307
Zhendong Tang <zhendongtt@gmail.com>
2022-02-13 21:54:57 -0500

    [Chapter-21] Create Razor view

---
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 21/Beginning of Chapter/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext context;
+        public HomeController(DataContext ctx)
+        {
+            context = ctx;
+        }
+        // GET
+        public async Task<IActionResult> Index(long id=1)
+        {
+            
+            //View will generate a IActionResult by which Response can be generated 
+            return View(await context.Products.FindAsync(id));
+        }
+    }
+}
