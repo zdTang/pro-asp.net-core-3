61d2f762720b2779b1435c6953340c90f0169b35
Zhendong Tang <zhendongtt@gmail.com>
2022-02-14 21:42:50 -0500

    [Chapter-22]TempData

---
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 22/Beginning of Chapter/WebApp/Controllers/CubedController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CubedController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return View("Cubed");
+        }
+
+        public IActionResult Cube(double num)
+        {
+            TempData["value"] = num.ToString();
+            TempData["result"]=Math.Pow(num,3).ToString();
+            // nameof() return a string constant
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
