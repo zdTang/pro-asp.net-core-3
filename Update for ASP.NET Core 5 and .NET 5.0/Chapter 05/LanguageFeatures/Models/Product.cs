5f2638411c7ba4b1b6e9d969dee47012caa04f2b
Zhendong Tang <zhendongtt@gmail.com>
2022-02-17 06:39:34 -0500

    [Chapter - 5]read-only approach two ---don't use [Constructor + optional parameter]
    use Initializer + Init

--- Update for ASP.NET Core 5 and .NET 5.0/Chapter 05/LanguageFeatures/Models/Product.cs
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 05/LanguageFeatures/Models/Product.cs
@@ -4,15 +4,15 @@
         // Constructor
         // assign a read-only property a default value
         
-        public Product(bool stock = true) {
-            InStock = stock;                   //  Init a read-only property
-        }
+        // public Product(bool stock = true) {
+        //     InStock = stock;                   //  Init a read-only property
+        // }
 
         public string Name { get; set; }
         public string Category { get; set; } = "Watersports";  // default value
         public decimal? Price { get; set; }
         public Product Related { get; set; }   // Notice the Type is "Product"
-        public bool InStock { get; }           // Read only, After initiation, it cannot be changed    
+        public bool InStock { get; init; } = true;          // Read only, After initiation, it cannot be changed    
         public bool NameBeginsWithS => Name?[0] == 'S';
 
         public static Product[] GetProducts() {
@@ -24,9 +24,11 @@
             };
             
             // assign another value to the read-only peoperty
-            Product lifejacket = new Product(false) {
+            //Product lifejacket = new Product(false) {
+            Product lifejacket = new Product {
                 Name = "Lifejacket",
-                Price = 48.95M
+                Price = 48.95M,
+                InStock = false
             };
 
             kayak.Related = lifejacket;     //  Notice how to define relationship within a Type definition itself ! 
