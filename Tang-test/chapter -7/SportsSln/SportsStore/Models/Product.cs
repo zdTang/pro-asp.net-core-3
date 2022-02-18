16d6da6fb6e6b8329e64a4c90595c0d92b0824c9
Zhendong Tang <zhendongtt@gmail.com>
2022-02-18 11:25:47 -0500

    [Chapter 7] Create Controller and View

---
+++ Tang-test/chapter -7/SportsSln/SportsStore/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsStore.Models
+{
+    public class Product
+    {
+        public long ProductId { set; get; }
+        public string Name { set; get; }
+        public string Description { set; get; }
+        [Column(TypeName="decimal(8,2)")]   //Mapping to database Column
+        public decimal Price { set; get; }
+        public string Category { set; get; }
+    }
+}
