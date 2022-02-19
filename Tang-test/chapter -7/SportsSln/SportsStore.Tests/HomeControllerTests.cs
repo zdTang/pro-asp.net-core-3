0652fe22c3b1794d9f38ec8ead8200eff42ffbb1
Zhendong Tang <zhendongtt@gmail.com>
2022-02-19 17:29:45 -0500

    [Chapter 7] unit test - pagination

--- Tang-test/chapter -7/SportsSln/SportsStore.Tests/HomeControllerTests.cs
+++ Tang-test/chapter -7/SportsSln/SportsStore.Tests/HomeControllerTests.cs
@@ -40,5 +40,40 @@ namespace SportsStore.Tests
             Assert.Equal("Bike",prodArray[1].Description);
 
         }
+
+        [Fact]
+        public void Can_Paginate()
+        {
+            //Arrange
+            //create fake data
+            var fake = new List<Product>
+            {
+                new Product() { ProductId = 1, Name = "p1" },
+                new Product() { ProductId = 2, Name = "p2" },
+                new Product() { ProductId = 3, Name = "p3" },
+                new Product() { ProductId = 4, Name = "p4" },
+                new Product() { ProductId = 5, Name = "p5" },
+                new Product() { ProductId = 6, Name = "p6" },
+                new Product() { ProductId = 7, Name = "p7" },
+                new Product() { ProductId = 8, Name = "p8" },
+                new Product() { ProductId = 9, Name = "p9" },
+                new Product() { ProductId = 10, Name = "p10" },
+                new Product() { ProductId = 11, Name = "p11" }
+            };
+            //Mock repository
+            var mock = new Mock<IStoreRepository>();
+            //Set up behavior
+            mock.Setup(m => m.Products).Returns(fake.AsQueryable<Product>());
+            // Use mock data
+            var controller = new HomeController(mock.Object);
+            controller.PageSize = 3;
+            //Action
+            var result = (controller.Index(2) as ViewResult)?.ViewData?.Model as IEnumerable<Product>;
+            //Assertion
+            var prodArray = result?.ToArray();
+            Assert.True(prodArray?.Length==3);
+            Assert.Equal("p4", prodArray[0].Name);
+            Assert.Equal("p5", prodArray[1].Name);
+        }
     }
 }
--- Tang-test/chapter -7/SportsSln/SportsStore/Controllers/HomeController.cs
+++ Tang-test/chapter -7/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -1,18 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+
 
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize { get; set; } = 4;          
 
         public HomeController(IStoreRepository repo)
         {
             repository = repo;
         }
+        /// <summary>
+        /// This method demonstrate how to do pagination
+        /// </summary>
+        /// <param name="productPage"></param>
+        /// <returns></returns>
         // GET
-        public IActionResult Index()=>View(repository.Products);
+        public IActionResult Index(int productPage = 1)
+        {
+            var model = repository
+                .Products.OrderBy(o => o.ProductId)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize);
+            return View(model);
+        }
         
     }
 }
