c8fea51478ad8c5eb55e7fed65be3b9aec0174c0
Zhendong Tang <zhendongtt@gmail.com>
2022-02-21 10:27:35 -0500

    [Chapter 8] Create ViewComponent

---
+++ Tang-test/chapter -7/SportsSln/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Components
+{
+    /// <summary>
+    /// Create a view component
+    /// Notice the name convention
+    /// this component will be "navigation-menu"
+    /// </summary>
+    public class NavigationMenuViewComponent:ViewComponent
+    {
+        public string Invoke()
+        {
+            return "Hello, Mike from the Nav View Component";
+        }
+    }
+}
