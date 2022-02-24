0df7b6eb11faac19ad12151939e3a7dbdd095bf0
zdtang <zhendongtt@gmail.com>
2022-02-23 20:51:27 -0500

    [Chapter 8] Create Cart class and test its function

---
+++ Tang-test/chapter -7/SportsSln/SportsStore.Tests/CartTests.cs
@@ -0,0 +1,85 @@
+﻿using System.Linq;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests
+{
+    public class CartTests
+    {
+        [Fact]
+        public void Can_Add_New_Lines()
+        {
+            //arrange
+            var p1 = new Product {ProductId = 1,Name = "P1"};
+            var p2 = new Product {ProductId = 2,Name = "P1"};
+            var cart = new Cart();
+            //action
+            cart.AddItem(p1,1);
+            cart.AddItem(p2,1);
+            cart.AddItem(p2,2);
+            //assert
+            Assert.Equal(2,cart.Lines.Count);
+            Assert.Equal(3,cart?.Lines.Where(x=>x.Product.ProductId==2)?.FirstOrDefault()?.Quantity);
+            Assert.Equal(1,cart?.Lines.Where(x=>x.Product.ProductId==1)?.FirstOrDefault()?.Quantity);
+        }
+
+        [Fact]
+        public void Can_Remove_Line()
+        {
+            //arrange
+            var p1 = new Product {ProductId = 1,Name = "P1"};
+            var p2 = new Product {ProductId = 2,Name = "P2"};
+            var cart = new Cart();
+            //action
+            cart.AddItem(p1,1);
+            cart.AddItem(p2,1);
+            cart.AddItem(p2,2);
+            cart.RemoveLine(p1);
+            var result = cart?.Lines.ToArray();
+            
+            //assert
+            Assert.Single(cart.Lines);
+            Assert.Equal(2,result[0].Product.ProductId);
+            Assert.Equal(3,result[0].Quantity);
+        }
+        
+        [Fact]
+        public void Can_Calculate_Cart_Total()
+        {
+            //arrange
+            var p1 = new Product {ProductId = 1,Name = "P1",Price = 100};
+            var p2 = new Product {ProductId = 2,Name = "P1",Price = 20};
+            var cart = new Cart();
+            //action
+            cart.AddItem(p1,1);
+            cart.AddItem(p2,1);
+            cart.AddItem(p2,2);
+            decimal result = cart.ComputedTotalValue();
+            
+            //assert
+
+            Assert.Equal(160,result);
+
+        }
+        
+        [Fact]
+        public void Can_Clear_Cart()
+        {
+            //arrange
+            var p1 = new Product {ProductId = 1,Name = "P1",Price = 100};
+            var p2 = new Product {ProductId = 2,Name = "P1",Price = 20};
+            var cart = new Cart();
+            //action
+            cart.AddItem(p1,1);
+            cart.AddItem(p2,1);
+            cart.AddItem(p2,2);
+            cart.Clear();
+            
+            //assert
+
+            Assert.Equal(0,cart?.Lines.Count);
+            Assert.Empty(cart?.Lines);
+
+        }
+    }
+}
---
+++ Tang-test/chapter -7/SportsSln/SportsStore/Models/Cart.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    /// <summary>
+    /// This class represents a Cart
+    /// A data collection is used as Container
+    /// A bunch of methods are created for modifying the Container
+    /// Is this the best practice ???
+    /// </summary>
+    
+    public class Cart
+    {
+        public List<CartLine> Lines { set; get; } = new List<CartLine>();
+
+        // For adding a new Item, check if the item was in the List
+        // if No, then add the new Item 
+        // if Yes, means the user want more same items so that increase the quantity
+        public void AddItem(Product product, int quantity)
+        {
+            CartLine line = Lines.Where(p => p.Product.ProductId == product.ProductId)?.FirstOrDefault();
+            if (line == null) // not in the List so that we put it in.
+            {
+                Lines.Add(new CartLine{Product = product,Quantity = quantity});
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Product product) => Lines.RemoveAll(l => l.Product.ProductId == product.ProductId);
+
+        public decimal ComputedTotalValue() => Lines.Sum(e => e.Quantity * e.Product.Price);
+
+        public void Clear() => Lines.Clear();
+    }
+
+    
+    /// <summary>
+    /// This class represents a product selected by the customer
+    /// </summary>
+    public class CartLine
+    {
+        public int CartLineId { set; get; }
+        public Product Product { set; get; }
+        public int Quantity { set; get; }
+    }
+}
