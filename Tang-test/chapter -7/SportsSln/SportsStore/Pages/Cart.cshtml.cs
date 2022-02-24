8e116fec28913dbe28438623fffcfc6655986e16
zdtang <zhendongtt@gmail.com>
2022-02-24 07:13:18 -0500

    [Chapter 8] Session + Cart
    1. use session to store Cart information
    2, session value serialization and deserialization
    3, Razor page pattern
    4, After Posting, Redirection

--- Tang-test/chapter -7/SportsSln/SportsStore/Pages/Cart.cshtml.cs
+++ Tang-test/chapter -7/SportsSln/SportsStore/Pages/Cart.cshtml.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
 
 namespace SportsStore.Pages
 {
-    public class Cart : PageModel
+    public class CartModel : PageModel
     {
-        public void OnGet()
+        private IStoreRepository repository;
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; }
+        
+        public CartModel(IStoreRepository repo)
         {
-            
+            repository = repo;
+        }
+        
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+
+        public IActionResult OnPost(long productID, string returnUrl)
+        {
+            Product product = repository.Products.FirstOrDefault(p => p.ProductId == productID);
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product,1);
+            HttpContext.Session.SetJson("cart",Cart);
+            // Redirects (Status302Found) to the current page with the specified routeValues.
+            // This post will update Session state
+            // After that, we need redirect the request to an active Page
+            return RedirectToPage(new {returnUrl = returnUrl});
         }
     }
 }
