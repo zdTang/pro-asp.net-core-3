ca558b670ab4d97d2cfa0e29b35166ffaa65d951
Zhendong Tang <zhendongtt@gmail.com>
2022-03-19 21:47:55 -0400

    [Chapter9- p224]- Creating Controller and View

---
+++ Tang-test/chapter -7/SportsSln/SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class OrderController : Controller
+    {
+        // Be aware here the "Order" we passed to view will be converted to Dynamic
+        // So that we need a @model in the razor to mapping a type
+        public IActionResult Checkout()=>View(new Order());
+        
+    }
+}
