4cae50cb08ba7166a7e4131f8667061e537081d3
Zhendong Tang <zhendongtt@gmail.com>
2022-04-03 22:24:57 -0400

    [Chapter 19 - P446] Understanding Action Method Results

--- Update for ASP.NET Core 5 and .NET 5.0/Chapter 19/Beginning of Chapter/WebApp/Controllers/ProductsController.cs
+++ Update for ASP.NET Core 5 and .NET 5.0/Chapter 19/Beginning of Chapter/WebApp/Controllers/ProductsController.cs
@@ -7,6 +7,9 @@ namespace WebApp.Controllers
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
+        // Here return an Array of Product
+        // don't serialize it manually, just return the Object Collection
+        // Framework will serialize it for you.
         [HttpGet]
         public IEnumerable<Product> GetProducts()
         {
@@ -16,6 +19,9 @@ namespace WebApp.Controllers
                 new Product(){Name="Product #2"}
             };
         }
+
+        // Here return a JSON, We don't need to Serialize Object
+        // The WebAPI controller will handle this !
 
         [HttpGet("{id}")]
         public Product GetProduct()
